bc38cdd4c28557023e1c5ab0bee1a813d3b7de65
zundrits <patricia.zundritsch@gmail.com>
2017-11-02 16:42:17 +0100

    Add in Var map first slide

--- Schiess_Zundritsch_BF.h
+++ Schiess_Zundritsch_BF.h
@@ -2,6 +2,16 @@
 <html>
  <body>
  
+<div id="map" style="width: 100%, height: 400px"></div>
+<script>
+var map = new ol.Map({target: 'map});
+    </script>
+    
+    map.setView(new ol.View({
+    center: [0,0],
+    zoom: 2
+    }));
+   
  
 </body>
 </html>
