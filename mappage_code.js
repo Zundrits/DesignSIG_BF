cac7460f614d11df67dea80d80d123c4f4403535
zundrits <patricia.zundritsch@gmail.com>
2017-11-16 16:04:16 +0100

    New version oj

--- mappage_code.js
+++ mappage_code.js
@@ -83,5 +83,59 @@
       })
     });
     	map.addLayer(places);
+    	
+    	// Add new vector layer to stock
+    	var source = new ol.source.Vector()
+    	var vector = new ol.layer.Vector({
+        source: source,
+        style: new ol.style.Style({
+          fill: new ol.style.Fill({
+            color: 'rgba(255, 255, 255, 0.2)'
+          }),
+          stroke: new ol.style.Stroke({
+            color: '#ffcc33',
+            width: 2
+          }),
+          image: new ol.style.Circle({
+            radius: 7,
+            fill: new ol.style.Fill({
+              color: '#ffcc33'
+            })
+          })
+        })
+      });
+      
+    
+    	
+    	
+    	//Add tool to modify layer 
+    	
+    	var modify = new ol.interaction.Modify({source: source});
+      map.addInteraction(modify);
+
+      var draw, snap; // global so we can remove them later
+      var typeSelect = document.getElementById('type');
+
+      function addInteractions() {
+        draw = new ol.interaction.Draw({
+          source: source,
+          type: /** @type {ol.geom.GeometryType} */ (typeSelect.value)
+        });
+        map.addInteraction(draw);
+        snap = new ol.interaction.Snap({source: source});
+        map.addInteraction(snap);
+
+      }
+
+      /**
+       * Handle change event.
+       */
+      typeSelect.onchange = function() {
+        map.removeInteraction(draw);
+        map.removeInteraction(snap);
+        addInteractions();
+      };
+
+      addInteractions();
 
 });
